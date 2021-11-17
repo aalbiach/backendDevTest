374de43e355ae80824f7ab9b5e63c52208921c4a
Alvaro Albiach <alvaro.albiach@gmail.com>
2021-11-17 11:54:52 +0100

    Fix: handle SocketTimeoutException

--- app/src/main/java/com/devtest/productapi/exception/GlobalExceptionHandler.java
+++ app/src/main/java/com/devtest/productapi/exception/GlobalExceptionHandler.java
@@ -5,21 +5,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.SocketTimeoutException;
 
 @RestControllerAdvice
 @Log4j2
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundClientException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> notFoundException(NotFoundClientException ex, WebRequest request, HttpServletRequest servletRequest) {
-        log.trace("Caught NotFoundClientException. Returning custom error");
+    public ResponseEntity<Object> handleNotFoundException(NotFoundClientException ex, WebRequest request, HttpServletRequest servletRequest) {
+        log.error("Caught NotFoundClientException. Returning custom error");
         final var status    = HttpStatus.NOT_FOUND;
         final var jsonError = new CustomJsonError(status, status.getReasonPhrase(), servletRequest.getRequestURI());
 
@@ -27,10 +26,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(InternalServerErrorClientException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Object> internalServerErrorClientException(InternalServerErrorClientException ex, WebRequest request, HttpServletRequest servletRequest) {
-        log.trace("Caught InternalServerErrorClientException. Returning custom error");
+    public ResponseEntity<Object> handleInternalServerErrorClientException(InternalServerErrorClientException ex, WebRequest request, HttpServletRequest servletRequest) {
+        log.error("Caught InternalServerErrorClientException. Returning custom error");
         final var status    = HttpStatus.INTERNAL_SERVER_ERROR;
+        final var jsonError = new CustomJsonError(status, status.getReasonPhrase(), servletRequest.getRequestURI());
+
+        return handleExceptionInternal(ex, jsonError, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(SocketTimeoutException.class)
+    public ResponseEntity<Object> handleSocketTimeoutException(SocketTimeoutException ex, WebRequest request, HttpServletRequest servletRequest) {
+        log.error("Caught SocketTimeoutException. Returning custom error");
+        final var status = HttpStatus.REQUEST_TIMEOUT;
         final var jsonError = new CustomJsonError(status, status.getReasonPhrase(), servletRequest.getRequestURI());
 
         return handleExceptionInternal(ex, jsonError, new HttpHeaders(), status, request);
